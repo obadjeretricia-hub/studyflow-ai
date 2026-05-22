feed0ecf46f335960c417461e2a4f93214569def
ObadjereAkpes <obadjeretricia@gmail.com>
2026-05-22 01:47:35 -0400

    Implement task management with React state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 import './App.css';
+import { useState } from 'react';
+
 function App() {
+  const [tasks, setTasks] = useState([
+      "Finish math assignment",
+      "Study data structures",
+      "Update Resume"
+    ]);
+  const [newTask, setNewTask] = useState("");
+  function addTask() {
+    if (newTask.trim() === "") return;
+
+    setTasks([...tasks, newTask]);
+    setNewTask("");
+  }
+  function deleteTask(indexToDelete){
+    setTasks(tasks.filter((task, index) => index !== indexToDelete));
+  }
   return (
     <div className="app">
       <header>
@@ -11,13 +28,20 @@ function App() {
       <section className="card">
         <h2>Today's Tasks</h2>
         <div className="task-input">
-          <input type="text" placeholder= "Add a new task.."/>
-          <button>Add Task</button>
+          <input type="text"
+           placeholder= "Add a new task..."
+           value={newTask}
+           onChange={(e) => setNewTask(e.target.value)}
+          />
+          <button onClick={addTask}>Add Task</button>
         </div>
           <ul className="task-list">
-            <li>Finish math assignment</li>
-            <li>Study data structures</li>
-            <li>Update Resume</li>
+            {tasks.map((task, index) =>(
+              <li key={index}>
+                <span>{task}</span>
+                <button onClick={() => deleteTask(index)}>Delete</button>
+              </li>
+            ))}
           </ul>
       </section>
       <section className="card">
